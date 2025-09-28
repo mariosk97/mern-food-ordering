771087a9b6eac6a1540909a42dd690d250339034
Marios <mariosk97@yahoo.gr>
2025-09-28 21:06:15 +0300

    Stop tracking node_modules and .env

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
@@ -10,8 +11,8 @@ const Layout = ({ children }: Props) => {
         <div className="flex flex-col min-h-screen">
             <Header />
             <Hero />
-
             <div className="container mx-auto flex-1 py-10"> {children} </div>
+            <Footer />
         </div>
         
     )
