cbae2a9c718a059fd776c756fd277934a3acc899
Marios <mariosk97@yahoo.gr>
2025-10-03 09:46:00 +0300

    user profile backend and frontend

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -45,4 +45,5 @@ const updateCurrentUser = async (req: Request, res: Response) => {
 
 export default {
     createCurrentUser,
+    updateCurrentUser,
 };
--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -3,6 +3,15 @@ import { auth } from "express-oauth2-jwt-bearer";
 import jwt from "jsonwebtoken";
 import User from "../models/user";
 
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
+
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE!,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL!,
@@ -15,8 +24,6 @@ export const jwtParse = async (req: Request, res: Response, next: NextFunction) 
   if(!authorization || !authorization.startsWith("Bearer")){
     return res.sendStatus(401);
   }
-
-  
 
   const token = authorization.split(" ")[1];
 
@@ -33,6 +40,10 @@ export const jwtParse = async (req: Request, res: Response, next: NextFunction) 
     if (!user) {
       return res.sendStatus(401);
     }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
   } catch (error) {
     return res.sendStatus(401);
   }
