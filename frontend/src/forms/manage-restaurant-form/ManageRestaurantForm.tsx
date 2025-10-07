4d4614631925f0244a03751e05788e3a31e68bf4
Marios <mariosk97@yahoo.gr>
2025-10-07 22:17:06 +0300

    manage restaurant page created

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -1,29 +1,42 @@
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
+import DetailsSection from "./DetailsSection";
 
 const formSchema = z.object({
-    restaurantName: z.string().min(1, "Restaurant name is required"),
-    city: z.string().min(1, "city is required"),
-    country: z.string().min(1, "country is required"),
-    deliveryPrice: z
+  restaurantName: z.string().min(1, "Restaurant name is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
+  deliveryPrice: z
+    .string()
+    .nonempty({ message: "Delivery price is required" })
+    .transform((val) => Number(val))
+    .refine((val) => !isNaN(val), { message: "Delivery price must be a number" })
+    .refine((val) => val >= 0, { message: "Delivery price must be at least 0" }),
+  estimatedDeliveryTime: z
+    .string()
+    .nonempty({ message: "Estimated delivery time is required" })
+    .transform((val) => Number(val))
+    .refine((val) => !isNaN(val), { message: "Estimated delivery time must be a number" })
+    .refine((val) => val >= 0, { message: "Estimated delivery time must be at least 0" }),
+  cuisines: z.array(z.string()).min(1, "Select at least one cuisine"),
+  menuItems: z.array(
+    z.object({
+      name: z.string().min(1, "Name is required"),
+      price: z
         .string()
-        .nonempty({ message: "Delivery price is required" }) 
-        .transform((val) => Number(val))                     
-        .refine((val) => !isNaN(val), { message: "Delivery price must be a number" })
-        .refine((val) => val >= 0, { message: "Delivery price must be minimum 0" }),
-    estimatedDeliveryTime: z
-        .string()
-        .nonempty({ message: "Estimated delivery time is required" }) 
-        .transform((val) => Number(val))                     
-        .refine((val) => !isNaN(val), { message: "Estimated delivery time must be a number" })
-        .refine((val) => val >= 0, { message: "Estimated delivery time must be minimum 0" }),
-    cuisines: z.array(z.string()).nonempty({message: "Please select at least one item"}),
-    menuItems: z.array(z.object({
-        name: z.string().min(1, "name is required"),
-        price: z.coerce.number().min(1, "price is required"),
-        })
-    ),    
-    imageFile: z.instanceof(File, {message:"Image is required"}),
-})
+        .nonempty({ message: "Price is required" })
+        .transform((val) => Number(val))
+        .refine((val) => !isNaN(val), { message: "Price must be a number" })
+        .refine((val) => val >= 0, { message: "Price must be at least 0" }),
+    })
+  ),
+  imageFile: z.instanceof(File, { message: "Image is required" }),
+});
+
+
+type restaurantFormData = z.infer<typeof formSchema>
 
 type Props = {
     onSave: (restaurantFormData: FormData)=> void;
@@ -31,7 +44,25 @@ type Props = {
 }
 
 const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            cuisines: [],
+            menuItems: [{name: "", price: ""}]
+        }
+    });
 
+    const onSubmit = () => {
+        //TODO convert formDataJson to a new formData object
+    }
+
+    return (
+        <Form {...form} >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
+                <DetailsSection />
+            </form>
+        </Form>
+    )
 }
 
 export default ManageRestaurantForm;
